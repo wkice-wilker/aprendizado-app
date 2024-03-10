681d607a5bc26725ac1066f94599ada991647453
OttoHapuc <dossantoshapuc@gmail.com>
2024-03-09 23:05:12 -0300

    feat: config vite

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,16 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
+    server: {
+        https: false,
+        host: true,
+        strictPort: true,
+        port: 7500,
+        hmr: {host: 'localhost', protocol: 'ws'},
+        watch: {
+          usePolling:true,
+        }
+      },
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
