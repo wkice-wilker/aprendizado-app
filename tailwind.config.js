59e13a9f2795ac32a7232d283aa196d0f43c7c2c
OttoHapuc <dossantoshapuc@gmail.com>
2024-03-09 23:40:25 -0300

    refactor: tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-        "./resources/**/*.blade.php",
-        "./resources/**/*.js",
-        "./resources/**/*.vue",
+        "./resources/**/*.{blade.php,js,ts,vue}",
       ],
   theme: {
     extend: {},
